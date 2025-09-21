725793bb9a23f29a527f11c1d5c9452ba2b89797
steesann <yrkiy.evgeny@gmail.com>
2025-09-21 17:40:30 +0300

    class station

--- EscControl/stations/models.py
+++ EscControl/stations/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class station(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название станции',
+    )
+    line = models.CharField(
+        max_length=100,
+        verbose_name='Название линии',
+    )
+    coordinates = models.CharField(
+        max_length=200,
+        null=True, blank=True,
+        verbose_name='Координаты станции',
+    )
+
+    class Meta:
+        verbose_name = 'Станция метро'
+        verbose_name_plural = 'Станции метро'
+        constrains = [
+            models.UniqueConstraint(fields=['name', 'line'], name='unique_station_per_line')
+        ]
+
+    def __str__(self):
+        return f"{self.name} ({self.line})"
