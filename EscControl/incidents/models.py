ed90628545ceef9400ac40b6178b7f788c293914
steesann <yrkiy.evgeny@gmail.com>
2025-09-21 19:01:33 +0300

    incidents/models.py

--- EscControl/incidents/models.py
+++ EscControl/incidents/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class IncidentType(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    name = models.CharField(
+        max_length=100,
+    )
+
+    description = models.TextField()
+
+class Incident(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    incidentType = models.ForeignKey(
+        'IncidentType',
+        on_delete=models.CASCADE
+    )
+    
+    escalator = models.ForeignKey(
+        'Escalator',
+        on_delete=models.CASCADE
+    )
+
+    timestamps = models.DateTimeField()
+    confidence = models.FloatField()
+
+    statusChoices = [
+        ('Open', 'Открыто'),
+        ('Closed', 'Закрыт')
+        ('inprogress', 'В работе'),
+    ]
+    status = models.CharField(
+        choices=statusChoices,
+        max_length=25,
+        default='Open',
+    )
+
+    screenshot = models.ImageField()
+    notes = models.TextField()
+
+
